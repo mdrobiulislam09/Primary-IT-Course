c5d51b0ff23137fbc3f2b39e9aec60fe34169abb
mdrobiulislam09 <robiul.rubin@gmail.com>
2022-11-21 16:35:57 +0600

    finaly done

--- src/PrivateRoute/Premium.js
+++ src/PrivateRoute/Premium.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 
 const Premium = () => {
     const data = useLoaderData();
     return (
         <div>
+            <Image variant="top" style={{ height: '22rem' }} src={data.picture} />
             {data.about}
         </div>
     );
